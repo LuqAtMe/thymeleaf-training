6c08a767075299a301b51d8ac0a5f40f7ccf5dc6
brzeznicki.lukasz@gmail.com <Gryfek69>
2020-02-16 13:41:01 +0100

    list iteration base on Car model

--- src/main/java/home/application/thymeleaftraining/CarController.java
+++ src/main/java/home/application/thymeleaftraining/CarController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
-    @GetMapping ("/car")
-    public String hello(Model model){
-        Car car = new Car ("BMW", "i8");
-        model.addAttribute("car", car);
+    @GetMapping("/car")
+    public String hello(Model model) {
+        Car car1 = new Car("BMW", "i8");
+        Car car2 = new Car("Mazda", "6");
+        Car car3 = new Car("Fiat", "126p");
+        Car car4 = new Car("Polonez", "Caro");
+        List<Car> carList = Arrays.asList(car1, car2, car3, car4);
+        model.addAttribute("cars", carList);
         return "car";
     }
 
