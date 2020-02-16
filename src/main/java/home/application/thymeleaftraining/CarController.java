cd1f69a4fc2875feb391fc547d351db13b53c991
brzeznicki.lukasz@gmail.com <Gryfek69>
2020-02-16 15:12:41 +0100

    adding a new Car to list, using post method on single page view

--- src/main/java/home/application/thymeleaftraining/CarController.java
+++ src/main/java/home/application/thymeleaftraining/CarController.java
@@ -3,22 +3,40 @@ package home.application.thymeleaftraining;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String hello(Model model) {
+    private List<Car> carList;
+
+    public CarController() {
         Car car1 = new Car("BMW", "i8");
         Car car2 = new Car("Mazda", "6");
         Car car3 = new Car("Fiat", "126p");
         Car car4 = new Car("Polonez", "Caro");
-        List<Car> carList = Arrays.asList(car1, car2, car3, car4);
+        carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        carList.add(car4);
+    }
+
+    @GetMapping("/car")
+    public String getCar(Model model) {
         model.addAttribute("cars", carList);
+        model.addAttribute("newCar", new Car());
         return "car";
+    }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car) {
+        carList.add(car);
+        return "redirect:/car";
     }
 
 }
